e01ae885cdbef3af265341110a434f6fa7b8e8ac
cr-marcstevens <github@marc-stevens.nl>
2021-07-22 10:58:56 +0200

    update bashrc

--- scripts/bashrc_auto_ssh_agent.sh
+++ scripts/bashrc_auto_ssh_agent.sh
@@ -12,16 +12,22 @@ if [ $? = 2 ]; then
         rm -rf $SSH_AUTH_SOCK 2>/dev/null
         ssh-agent > ~/.ssh-agent-socket
         source ~/.ssh-agent-socket
+else
+	echo "Not starting SSH agent: $?"
 fi
 
 # print message about ssh-agent and list keys
 ssh-add -l
 
-# if not using ssh and list is empty then add aliases to automatically add keys when first running ssh or scp
+# if not using ssh and list is empty then add aliases to automatically add keys when first running ssh, scp, or git
 if [ -z "$SSH_TTY" ]; then
   ssh-add -l > /dev/null
   if [ $? -ne 0 ] ; then
-    alias ssh='ssh-add -l > /dev/null || ssh-add && unalias ssh scp ; ssh'
-    alias scp='ssh-add -l > /dev/null || ssh-add && unalias scp ssh ; scp'
+    alias ssh='ssh-add -l > /dev/null || ssh-add && unalias ssh scp git ; ssh'
+    alias scp='ssh-add -l > /dev/null || ssh-add && unalias scp ssh git ; scp'
+    alias git='ssh-add -l > /dev/null || ssh-add && unalias scp ssh git ; git'
   fi
 fi
+
+# only if you also use gpg to sign commits for git
+# export GPG_TTY=$(tty)
